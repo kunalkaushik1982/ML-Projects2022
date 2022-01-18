61d04e6c03fe128eef59b2bdeed4f3ab8ced0588
Kunal Kaushik <kunkaush1@publicisgroupe.net>
2022-01-18 23:22:42 +0530

    Pyhton Data Types

--- TestingTheWaters/LearnPython/BasicsOfPython.py
+++ TestingTheWaters/LearnPython/BasicsOfPython.py
@@ -1,4 +1,50 @@
+import string
+from xmlrpc.client import Boolean
+
+
 print("Hello 2022", end='')
 # firstname=input("Enter your first name :")
 # print("Your Name is: ", firstname)
 print("Kunal","Mrinal","Harsh","Aarya", sep='#')
+
+
+
+# Python Support 3 categories of datatypes:
+#     Basic Types - Integer, Float, Boolean,string, complex
+#     Container Types - List, Set, Dictionary, Tuples
+#     User Defined Type - Class
+#Integer
+print(4)
+print(5000000000000000000000000000000000000000000000000000000000000000000000000000000)
+print(1e309)
+
+#Float
+print(4.5)
+print(1.7e307)
+print(1.7e308)
+print(1.7e309)
+
+#Boolean
+print(True)
+print(False)
+
+#Complex
+print(4+5j)
+print(4+9j)
+
+#String
+print('Kunal')
+print("Kunal")
+print("""Kunal""")
+
+#List
+print([1,2,3,4,5])
+
+#Tuples
+print((1,2,3,4,5))
+
+#Set
+print({1,2,3,4,5})
+
+#Dictionary
+print({"Name":"Kunal","Age":30,"Gender":"Male"})
